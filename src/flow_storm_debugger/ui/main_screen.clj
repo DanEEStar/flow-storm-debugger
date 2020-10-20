a28455cc427b657528d06bdea6bc0c60d53867f4
Juan Monetta <jpmonettas@gmail.com>
2020-10-20 16:40:22 -0300

    Resizable panes (split-panes)

--- src/flow_storm_debugger/ui/main_screen.clj
+++ src/flow_storm_debugger/ui/main_screen.clj
@@ -1,6 +1,11 @@
 (ns flow-storm-debugger.ui.main-screen
   (:require [cljfx.api :as fx]
-            [clojure.core.cache :as cache]))
+            [clojure.core.cache :as cache]
+            [cljfx.prop :as fx.prop]
+            [cljfx.mutator :as fx.mutator]
+            [cljfx.lifecycle :as fx.lifecycle]
+            [flow-storm-debugger.highlighter :as highlighter])
+   (:import [javafx.scene.web WebView]))
 
 (def *state
   (atom (fx/create-context {:counter 0
@@ -35,22 +40,88 @@
 (defn custom-fx [v dispatch!]
   (println "FX called with " v))
 
-(defn root [{:keys [fx/context]}]
+(def ext-with-html
+  (fx/make-ext-with-props
+    {:html (fx.prop/make
+             (fx.mutator/setter #(.loadContent (.getEngine ^WebView %1) %2))
+             fx.lifecycle/scalar)}))
+
+(defn code-browser []
+  {:fx/type ext-with-html
+   :props {:html (str "<pre>"
+                      (highlighter/highlight-expr "(->> (range 10)\n     (map inc)\n     (filter odd?) \n     (reduce +))" [3] "<b>" "</b>")
+                      "</pre>")}
+   :desc {:fx/type :web-view}})
+
+(defn bottom-bar [{:keys []}]
+  {:fx/type :pane
+   :pref-height 50
+   :style {:-fx-background-color :orange}
+   :children []})
+
+(defn main-screen [{:keys [fx/context]}]
   {:fx/type :stage
    :showing true
+   :width 1000
+   :height 1000
    :scene {:fx/type :scene
-           :root {:fx/type :h-box
-                  :children [{:fx/type :label
-                              :on-mouse-clicked {:event/type :clickeddddd
-                                                 :some-data [42 42]}
-                              :text (fx/sub-ctx context all-sub)}]}}})
+           :root {:fx/type :border-pane                  
+                  :center {:fx/type :split-pane
+                           :items [{:fx/type :split-pane
+                                    :orientation :vertical
+                                    :items [{:fx/type :pane
+                                             :style {:-fx-background-color :red}
+                                             :children []}
+                                            {:fx/type :pane
+                                             :style {:-fx-background-color :green}
+                                             :children []}]}
+                                   {:fx/type :split-pane
+                                    :orientation :vertical
+                                    :items [{:fx/type :pane
+                                             :style {:-fx-background-color :blue}
+                                             :children []}
+                                            {:fx/type :pane
+                                             :style {:-fx-background-color :yellow}
+                                             :children []}]}]}
+                  #_{:fx/type :grid-pane
+                           :column-constraints [{:fx/type :column-constraints :max-width 100}]
+                           :row-constraints [{:fx/type :row-constraints :max-height 100}]
+                           :children [{:fx/type :pane
+                                       :grid-pane/column 0
+                                       :grid-pane/row 0
+                                       :grid-pane/hgrow :always
+                                       :grid-pane/vgrow :always                                       
+                                       :style {:-fx-background-color :red}
+                                       :children []}
+                                      {:fx/type :pane
+                                       :grid-pane/column 1
+                                       :grid-pane/row 0
+                                       :grid-pane/hgrow :always
+                                       :grid-pane/vgrow :always
+                                       :style {:-fx-background-color :green}
+                                       :children []}
+                                      {:fx/type :pane
+                                       :grid-pane/column 1
+                                       :grid-pane/row 1
+                                       :grid-pane/hgrow :always
+                                       :grid-pane/vgrow :always
+                                       :style {:-fx-background-color :blue}
+                                       :children []}
+                                      {:fx/type :pane
+                                       :grid-pane/column 0
+                                       :grid-pane/row 1
+                                       :grid-pane/hgrow :always
+                                       :grid-pane/vgrow :always
+                                       :style {:-fx-background-color :yellow}
+                                       :children []}]}
+                  :bottom {:fx/type bottom-bar}}}})
 
 (def renderer
   (fx/create-renderer
     :middleware (comp
                   ;; Pass context to every lifecycle as part of option map
                   fx/wrap-context-desc
-                  (fx/wrap-map-desc (fn [_] {:fx/type root})))
+                  (fx/wrap-map-desc (fn [_] {:fx/type main-screen})))
     :opts {:fx.opt/type->lifecycle #(or (fx/keyword->lifecycle %)
                                         ;; For functions in `:fx/type` values, pass
                                         ;; context from option map to these functions
@@ -66,7 +137,7 @@
 
 (comment
   (fx/mount-renderer *state renderer)
-
+(renderer)
   (swap! *state fx/swap-context update :counter inc)
   (swap! *state fx/swap-context update :tasks conj {:name "AAA"})
   )
